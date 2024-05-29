0a0691a2694a3e0b46867111028e0af4ee4e8dbe
Mecasu <melihcansunay@live.com>
2024-05-29 15:02:28 +0300

    task done.

---
+++ src/main/java/org/example/arge/ElectricCar.java
@@ -0,0 +1,31 @@
+package org.example.arge;
+
+public class ElectricCar extends CarSkeleton {
+    private double avgKmPerCharge;
+    private int batterySize;
+
+    public ElectricCar(String name, String description, double avgKmPerCharge, int batterySize) {
+        super(name, description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    public double getAvgKmPerCharge() {
+        return avgKmPerCharge;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    @Override
+    public String drive() {
+        return "Electric Car is driving";
+    }
+
+    @Override
+    public String startEngine() {
+        runEngine();
+        return "Electric Car's engine started.";
+    }
+}
